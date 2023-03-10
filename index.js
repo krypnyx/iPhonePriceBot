e13472b6201a6f2ffd4a5179a1e598fd140eb008
Nitin <56484699+krypnyx@users.noreply.github.com>
2023-03-11 02:38:14 +0530

    changes

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const mongoose = require('mongoose');
 
 // Connect to database
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to database'))
-    .catch((error) => console.error(error));
+  .then(() => console.log('Connected to database'))
+  .catch((error) => console.error(error));
 
 // Define schema for subscribers
 const subscriberSchema = new mongoose.Schema({
-    chatId: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    subscribed: {
-        type: Boolean,
-        default: true
-    }
+  chatId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  subscribed: {
+    type: Boolean,
+    default: true
+  }
 });
 
 // Define model for subscribers
@@ -28,51 +28,55 @@ const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
 // Handle /start command
 bot.onText(/\/start/, async (msg) => {
-    const chatId = msg.chat.id;
+  const chatId = msg.chat.id;
 
-    // Check if user is already subscribed
+  try {
     const subscriber = await Subscriber.findOne({ chatId });
-
     if (subscriber) {
-        bot.sendMessage(chatId, 'You are already subscribed to daily updates.');
+      bot.sendMessage(chatId, 'You are already subscribed to daily updates.');
     } else {
-        // Create new subscriber
-        const newSubscriber = new Subscriber({ chatId });
-        await newSubscriber.save();
-        bot.sendMessage(chatId, 'You have subscribed to daily updates.');
+      const newSubscriber = new Subscriber({ chatId });
+      await newSubscriber.save();
+      bot.sendMessage(chatId, 'You have subscribed to daily updates.');
     }
+  } catch (error) {
+    console.error(error);
+    bot.sendMessage(chatId, 'Something went wrong. Please try again later.');
+  }
 });
 
 // Handle /stop command
 bot.onText(/\/stop/, async (msg) => {
-    const chatId = msg.chat.id;
+  const chatId = msg.chat.id;
 
-    // Update subscriber
-    const subscriber = await Subscriber.findOne({ chatId });
-    subscriber.subscribed = false;
-    await subscriber.save();
-
-    bot.sendMessage(chatId, 'You have unsubscribed from daily updates.');
+  try {
+    const subscriber = await Subscriber.findOneAndUpdate({ chatId }, { subscribed: false });
+    if (subscriber) {
+      bot.sendMessage(chatId, 'You have unsubscribed from daily updates.');
+    } else {
+      bot.sendMessage(chatId, 'You are not subscribed to daily updates.');
+    }
+  } catch (error) {
+    console.error(error);
+    bot.sendMessage(chatId, 'Something went wrong. Please try again later.');
+  }
 });
 
 // Set up admin panel
 bot.onText(/\/admin/, (msg) => {
   const chatId = msg.chat.id;
-  console.log('ID',chatId);
-  // Check if user is admin
+
   if (chatId === process.env.ADMIN_CHAT_ID) {
-    console.log('HI');
-      // Show admin panel
-      bot.sendMessage(chatId, 'Welcome to the admin panel. What would you like to do?', {
-          reply_markup: {
-              keyboard: [
-                  ['Update bot settings'],
-                  ['Manage user accounts']
-              ]
-          }
-      });
+    bot.sendMessage(chatId, 'Welcome to the admin panel. What would you like to do?', {
+      reply_markup: {
+        keyboard: [
+          ['Update bot settings'],
+          ['Manage user accounts']
+        ]
+      }
+    });
   } else {
-      bot.sendMessage(chatId, 'You are not authorized to access the admin panel.');
+    bot.sendMessage(chatId, 'You are not authorized to access the admin panel.');
   }
 });
 
@@ -80,37 +84,30 @@ bot.onText(/\/admin/, (msg) => {
 bot.onText(/Update bot settings/, (msg) => {
   const chatId = msg.chat.id;
 
-  // Check if user is admin
   if (chatId === process.env.ADMIN_CHAT_ID) {
-      // Prompt user for new settings
-      bot.sendMessage(chatId, 'Enter the new bot settings:');
+    bot.sendMessage(chatId, 'Enter the new bot settings:');
   } else {
-      bot.sendMessage(chatId, 'You are not authorized to perform this action.');
+    bot.sendMessage(chatId, 'You are not authorized to perform this action.');
   }
 });
 
-bot.onText(/Manage user accounts/, (msg) => {
-  const chatId = msg.chat.id;
-
-  // Check if user is admin
-  if (chatId === process.env.ADMIN_CHAT_ID) {
+bot.onText(/Manage user accounts/, async (msg) => {
+    const chatId = msg.chat.id;
+  
+    // Check if user is admin
+    if (chatId === process.env.ADMIN_CHAT_ID) {
       // Show list of user accounts
       bot.sendMessage(chatId, 'List of user accounts:');
-      Subscriber.find({}, (err, subscribers) => {
-          if (err) {
-              console.log(err);
-          } else {
-              subscribers.forEach((subscriber) => {
-                  bot.sendMessage(chatId, `${subscriber.chatId} - Subscribed: ${subscriber.subscribed}`);
-              });
-          }
-      });
-  } else {
-      bot.sendMessage(chatId, 'You are not authorized to perform this action.');
-  }
-});
-
-
-bot.on('message', (msg) => {
-  console.log(msg.chat.id);
-});
+      try {
+        const subscribers = await Subscriber.find({});
+        subscribers.forEach((subscriber) => {
+          bot.sendMessage(chatId, `${subscriber.chatId} - Subscribed: ${subscriber.subscribed}`);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+        bot.sendMessage(chatId, 'You are not authorized to perform this action.');
+    }
+  });
+  
